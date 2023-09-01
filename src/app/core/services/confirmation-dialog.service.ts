624355e7632b5ea29eb173576b251ced4180446c
Ben Visintainer <ben.visintainer@me.com>
2023-09-01 15:06:57 -0600

    Added exitAnimationDuration and set to 0 to prevent visual flashes when closing the dialog.

--- src/app/core/services/confirmation-dialog.service.ts
+++ src/app/core/services/confirmation-dialog.service.ts
@@ -28,7 +28,8 @@ export class ConfirmationDialogService {
   public showDialog(args: ConfirmationDialogArgs): Observable<boolean> {
     this._dialogRef = this._dialog.open(ConfirmationDialogComponent, {
       data: args,
-      enterAnimationDuration: 0
+      enterAnimationDuration: 0,
+      exitAnimationDuration: 0
     })
 
     return this._dialogRef.afterClosed() as Observable<boolean>;
